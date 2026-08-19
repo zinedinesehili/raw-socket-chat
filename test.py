6e2ccd0c0cd3ac018b60cf0ca1c9259ef60716aa
zinedinesehili <zs6496@rit.edu>
2026-08-19 15:52:22 -0400

    removed random file

--- test.py
+++ test.py
@@ -1,5 +1,31 @@
-import datetime
+import socket
+import threading
 
-current = datetime.datetime
 
-print(current.now)
+PORT = 12340
+
+def recieve_message(active_socket):
+    while True: 
+        data = active_socket.recv(1024)
+        if not data:
+            break
+        print(f"{data.decode()}")
+
+def main():
+    ip = input("Enter the address you want to connect to")
+    username = input("Enter your username: ")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((ip, PORT))
+        thread = threading.Thread(target=recieve_message, args=(s,))
+        thread.start()
+
+        s.sendall(username.encode("utf-8"))
+        while True:
+            message = input()
+            s.sendall(message.encode("utf-8"))
+            if message == "/quit":
+                thread.join()
+                break
+
+if __name__ == "__main__":
+    main()
